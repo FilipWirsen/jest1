3481923d9ba73c020f1747d2805c4d4e41f8bc69
FilipWirsen <Filip.wirsen99@gmail.com>
2022-04-07 12:16:50 +0000

    Mock testing

---
+++ scripts/tests/calc.js
@@ -0,0 +1,11 @@
+function addition(num, num2) {
+    if (Number.isInteger(num && num2)){
+        return num + num2;
+    } else {
+        console.log(Error);
+        return Error;
+    }
+    
+}
+
+module.exports = addition;
---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,24 @@
+const addition = require("./calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("Should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73)
+        });
+        test("Should return error for int + string", () => {
+            expect(addition(10, "donkey")).toBe(Error);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
